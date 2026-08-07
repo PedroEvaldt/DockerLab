6a4cb40de7b41a002270875498a99e63bbe863cb
PedroEvaldt <pedrofossati067@gmail.com>
2026-08-07 13:57:56 -0300

    adicionar redis

--- internal/store/redis.go
+++ internal/store/redis.go
@@ -1,6 +1,11 @@
 package store
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"strings"
+
 	"github.com/PedroEvaldt/shortener/internal/config"
 	"github.com/redis/go-redis/v9"
 )
@@ -14,4 +19,67 @@ func NewRedisStore(cfg config.Config) *RedisStore {
 	return &RedisStore{
 		client: client,
 	}
+}
+
+func (r *RedisStore) IncrementClick(ctx context.Context, code string) error {
+	err := r.client.Incr(ctx, fmt.Sprintf("clicks:%s", code)).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *RedisStore) PendingClicks(ctx context.Context, code string) (int64, error) {
+	val, err := r.client.Get(ctx, fmt.Sprintf("clicks:%s", code)).Int64()
+	if errors.Is(err, redis.Nil) {
+		return 0, nil
+	}
+	if err != nil {
+		return 0, err
+	}
+	return val, nil
+}
+
+func (r *RedisStore) DrainClicks(ctx context.Context) (map[string]int64, error) {
+	clicks := make(map[string]int64)
+	var cursor uint64
+
+	for {
+		var keys []string
+		var err error
+
+		keys, cursor, err = r.client.Scan(ctx, cursor, "clicks:*", 100).Result()
+		if err != nil {
+			return nil, err
+		}
+		for _, key := range keys {
+			val, err := r.client.GetDel(ctx, key).Int64()
+			if errors.Is(err, redis.Nil) {
+				return nil, nil
+			}
+			if err != nil {
+				return nil, err
+			}
+			code := strings.TrimPrefix(key, "clicks:")
+			clicks[code] = val
+		}
+		if cursor == 0 {
+			break
+		}
+	}
+	return clicks, nil
+}
+
+func (r *RedisStore) Ping(ctx context.Context) error {
+	if err := r.client.Ping(ctx).Err(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *RedisStore) Close() error {
+	if err := r.client.Close(); err != nil {
+		return err
+	}
+	return nil
 }
